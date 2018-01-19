67c7c8b404f132ca8811000110b85dfd46151abd
e <2398708910@qq.com>
2018-01-19 21:23:18 +0800

    首页，登录，注册动态页面

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ var sass = require("gulp-sass");
 
 //添加copyHtml任务,保存到指定目录
 gulp.task("copyHtml",function(){
-	gulp.src("index.html").pipe(gulp.dest("D:/phpStudy/WWW/haveMe"));
+	gulp.src("*.html").pipe(gulp.dest("D:/phpStudy/WWW/haveMe"));
 
 });
+
 //添加copyImg任务，保存到指定目录
 gulp.task("copyImg",function(){
 	gulp.src("img/**/*").pipe(gulp.dest("D:/phpStudy/WWW/haveMe/img"));
@@ -16,17 +17,17 @@ gulp.task("copyImg",function(){
 });
 //copyJS任务，保存到指定目录
 gulp.task("copyJs",function(){
-	gulp.src("js/index.js").pipe(gulp.dest("D:/phpStudy/WWW/haveMe/js"));
+	gulp.src("js/**.js").pipe(gulp.dest("D:/phpStudy/WWW/haveMe/js"));
 });
 //将转换成的css文件放到指定目录下
 gulp.task("moveCss",function(){
-	gulp.src("css/index.css").pipe(gulp.dest("D:/phpStudy/WWW/haveMe/css"));
+	gulp.src("css/**.css").pipe(gulp.dest("D:/phpStudy/WWW/haveMe/css"));
 });
 
 
 //添加sass转换到css任务
 gulp.task("sass",function(){
-	gulp.src("css/index.scss")
+	gulp.src("css/**.scss")
 	.pipe(sass())
 	.pipe(gulp.dest("css"));
 });
@@ -35,9 +36,9 @@ gulp.task("sass",function(){
 
 //添加自动监听任务功能
 gulp.task("watch",function(){
-	gulp.watch("index.html",["copyHtml"]);
+	gulp.watch("*.html",["copyHtml"]);
 	gulp.watch("img/**/*",["copyImg"]);
-	gulp.watch("css/index.scss",["sass"]);
-	gulp.watch("css/index.css",["moveCss"]);
-	gulp.watch("js/index.js",["copyJs"]);
+	gulp.watch("css/**.scss",["sass"]);
+	gulp.watch("css/**.css",["moveCss"]);
+	gulp.watch("js/**.js",["copyJs"]);
 });
